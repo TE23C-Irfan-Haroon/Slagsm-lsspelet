75a9254ce666562c00c5021df9ca3c8e02211462
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2024-09-26 13:11:05 +0200

    v1.0

--- Slagsmålsspelet/Program.cs
+++ Slagsmålsspelet/Program.cs
@@ -1,9 +1,55 @@
 ﻿
-int chance = Random.Shared.Next(10);
 
-int hp = 100;
+int player1hp = 100;
+int player2hp = 100; 
 
-while (hp > 0)
+int enemyMaxDamage = 10;
+
+string username;
+
+Console.WriteLine("Write your username for your fighter:");
+username = Console.ReadLine();
+
+string player1 = username; 
+
+Console.WriteLine("Choose who you want to figth:");
+Console.WriteLine("Level 1:Mario");
+Console.WriteLine("Level 2:Ironman");
+Console.WriteLine("Level 3:Kratos");
+Console.WriteLine("Choose between 1,2 or 3");
+
+string player2 = Console.ReadLine().ToLower();
+
+if (player2 == "a")
 {
+    player2 = "Kratos";
+    enemyMaxDamage = 40;
+}
+else if (player2 == "b")
+
+{
+    player2 = "Scooby doo";
+}
+else if (player2 == "c")
+{
+    player2 = "Ironman";
+}
+
+while (player1hp > 0 && player2hp > 0)
+{
+    Console.WriteLine("----------====NEW ROUND====----------");
+    Console.WriteLine($"{player1}: {player1hp} {player2}: {player2hp}");
+
+
+    int player1damage = Random.Shared.Next(20);
+    player2hp -= player1damage; 
     
+    int player2damage = Random.Shared.Next(enemyMaxDamage);
+    player1hp -= player2damage;
+
+    Console.WriteLine($"{player1} did {player1damage} damage to {player2}");
+    Console.WriteLine($"{player2} did {player2damage} damage to {player1}");
+
+
+    Console.ReadLine();
 }
